021ae573d5176a6be4dce97312e00b8217c8f76d
đức khiêm <66940340+maiduckhiem@users.noreply.github.com>
2021-08-31 22:27:01 -0700

    khiemmd

---
+++ lab4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            Console.Write("moi nhap so sao:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write("");
+                }
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+//tam giác sao
